70397268bc3323ce918f16c808f6e3f26bf7b0c9
yy <447898599@qq.com>
2022-03-12 16:08:00 +0800

    1

--- src/main/java/com/module/IntelliFL.java
+++ src/main/java/com/module/IntelliFL.java
@@ -12,26 +12,34 @@ public class IntelliFL extends Bean implements IProcessModule{
         super(config);
     }
 
-    private String command1 = "java -cp @:BUILD_TEST@:@:BUILD@ -javaagent:intelliFL.jar=covLevel=meth-cov org.junit.runner.JUnitCore org.junit.runner.JUnitCore @:TEST_CLASS@ \r\n";
+    private String command1 = "java -cp @:BUILD_TEST@:@:BUILD@ -javaagent:intelliFL.jar=covLevel=meth-cov org.junit.runner.JUnitCore @:TEST_CLASS@ \r\n";
 
     private String command2 = "java -cp /home/yy/intelliFL/intelliFL.jar set.intelliFL.cg.CallGraphBuilder @:BUILD@ \r\n";
+
+    private String command3 = "if [ ! -d /home/yy/MBFL/@:PROJECTID@/@:PROJECTID@-@:BUGID@/inteliFL ];then\n" +
+            "        mkdir -p /home/yy/MBFL/@:PROJECTID@/@:PROJECTID@-@:BUGID@/inteliFL\n" +
+            "fi\n" +
+            "\n" +
+            "mv intelliFL/intelliFL-meth-cov/* /home/yy/MBFL/@:PROJECTID@/@:PROJECTID@-@:BUGID@/inteliFL";
     @Override
     public void process(Runtime runTime) throws Exception {
         String buildPath = Utils.getSrcDir(runTime, projectPath);
         String buildTestPath = Utils.getBuildTest(runTime, projectPath);
-        String[] allTestArray = Utils.getAllTestArray(runTime, projectPath, projectId, bugId);
+        String[] allTestArray = Utils.getAllTestArray(runTime, projectPath);
 
         File methcovFile = new File(System.getProperty("user.home") + File.separator + "intelliFL" + File.separator + projectId + "_methcov.sh");
         StringBuilder methcov = new StringBuilder();
         for (String test : allTestArray){
+            System.out.println("testClass:" + test);
             methcov.append(command1.replace("@:BUILD_TEST@", buildTestPath).
                     replace("@:BUILD@", buildPath)
                     .replace("@:TEST_CLASS@", test));
         }
-        FileUtils.writeStringToFile(methcovFile, methcov.toString(), true);
+        methcov.append("\n").append(command3.replace("@:PROJECTID@", projectId).replace("@:BUGID@", bugId));
+        FileUtils.writeStringToFile(methcovFile, methcov.toString(), false);
 
         File callGraphFile = new File(System.getProperty("user.home") + File.separator + "intelliFL" + File.separator + projectId + "_callgraph.sh");
-        FileUtils.writeStringToFile(callGraphFile, command2.replace("@:BUILD@", buildPath));
+        FileUtils.writeStringToFile(callGraphFile, command2.replace("@:BUILD@", buildPath), false);
     }
 
     @Override
